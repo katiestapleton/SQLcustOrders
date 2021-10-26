a805ba05b8541e643215147b3d068037faeab505
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-10-25 23:32:08 -0500

    cleaned up formatting

--- SQLfiles/dbCreatation.sql
+++ SQLfiles/dbCreatation.sql
@@ -1,4 +1,9 @@
-create table customers (
+--Author: Katie Stapleton
+--Customer Orders DB Project
+--File: DBCreation.sql
+--Location: https://github.com/katiestapleton/SQLcustOrders/tree/development
+
+CREATE TABLE customers (
 CustomerID INT,
 FirstName varchar(25),
 LastName varchar(25),
@@ -10,7 +15,7 @@ Telephone varchar(15),
 PRIMARY KEY(CustomerID)
 );
 
-create table orders (
+CREATE TABLE orders (
 OrderID INT,
 CustomerID INT,
 SKU varchar(20),
@@ -26,35 +31,35 @@ Status VARCHAR (15),
 Reason VARCHAR(25),
 PRIMARY KEY(RMAID));
 
-alter table customers RENAME Collaborator;
-alter table Collaborator RENAME customers ;
+ALTER TABLE customers RENAME Collaborator;
+ALTER TABLE Collaborator RENAME customers ;
 
 LOAD DATA INFILE '/home/codio/workspace/customers.csv'
 INTO TABLE customers
-Fields terminated by ','
-Enclosed by '"' lines terminated by '\n';
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' lines terminated by '\n';
 
 LOAD DATA INFILE '/home/codio/workspace/orders.csv'
 INTO TABLE orders
-Fields terminated by ','
-Enclosed by '"' lines terminated by '\n';
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' lines terminated by '\n';
 
 LOAD DATA INFILE '/home/codio/workspace/rma.csv'
 INTO TABLE RMA
-Fields terminated by ','
-Enclosed by '"' lines terminated by '\n';
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' lines terminated by '\n';
 
-select count(*) from customers
+SELECT COUNT(*) FROM customers
 INNER JOIN orders
-on customers.CustomerID = Orders.CustomerID
-where Customers.City = 'Brooklyn';
+ON customers.CustomerID = Orders.CustomerID
+WHERE Customers.City = 'Brooklyn';
 
-select count(*) from customers
+SELECT COUNT(*) FROM customers
 INNER JOIN orders
-on customers.CustomerID = orders.CustomerID
-where customers.State = 'DE';
+ON customers.CustomerID = orders.CustomerID
+WHERE customers.State = 'DE';
 
-insert into customers
+INSERT into customers
 values
 (1000501, 'Black', 'Widow', '6 Avengers Blvd', 'Infinity', 'Washington', 10242, '624-555-2483'),
 (1000502, 'Kylo', 'Ren', '202 Sith Street', 'First Order', 'Michigan', 27901, '827-555-6527'),
@@ -65,7 +70,7 @@ values
 (1000507, 'Hikaru', 'Sulu','2290 Excelsior Street', '1966 ', 'Starship', 'Delware', 33458, '372-555-8207'),
 (1000508, 'Peter', 'Parker', '167 Webbing Avenue', 'Brooklyn', 'New York', 90501, '674-555-5678');
 
-insert into orders
+INSERT into orders
 values
 (1203001, 1000501, 'CAF-533', 'Cranberries - Dried'),
 (1203002, 1000504, 'ACT-222', 'Appetizer - Calamari'),
@@ -73,25 +78,25 @@ values
 (1203005, 1000507, 'NES-242', 'Soup - Tomato');
 (1203006, 1000507, 'GED-347', 'Cheese - Grana Padano');
 
-select count(*) from customers
-inner join orders
+SELECT COUNT(*) FROM customers
+INNER JOIN orders
 ON customers.CustomerID = orders.CustomerID
-where customers.City = 'Brooklyn';
+WHERE customers.City = 'Brooklyn';
 
-update RMA 
-set Status = 'Complete', 
+UPDATE RMA 
+SET Status = 'Complete', 
 Step = 'Credit Customer Account' 
-where OrderID = 5175;
+WHERE OrderID = 5175;
 
-select Status, Step 
-from RMA
-where OrderID = 5175;
+SELECT Status, Step 
+FROM RMA
+WHERE OrderID = 5175;
 
-delete from RMA
-where Reason LIKE 'R%';
+DELETE FROM RMA
+WHERE Reason LIKE 'R%';
 
-select * from orders
+SELECT* FROM orders
 INTO outfile'/home/codio/workspace/orders-output.csv'
 FIELDS terminated by ','
-enclosed by '"'
+ENCLOSED BY '"'
 lines terminated by '/n';
